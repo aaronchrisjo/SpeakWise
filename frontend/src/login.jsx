0dc2327c2a20ec12d9b481071b74019a46d1d012
Aaron CJ <aaroncj089@gmail.com>
2023-06-02 14:16:44 +0530

    first

---
+++ frontend/src/login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-white to-white">
+      <img className=" " src="speakwise-png.png" alt="" />
+      <div className="w-full max-w-md bg-white rounded-lg shadow-xl p-8">
+        <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
